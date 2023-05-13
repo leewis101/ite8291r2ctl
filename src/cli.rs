2093655231b7d616c60d8034018aa5cf3027a831
leewis <simpleiland@gmail.com>
2023-05-13 12:00:37 +0800

    refactor: parse color

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,6 @@
 use std::fmt;
-use std::str::FromStr;
 
-use clap::{Parser, ValueEnum};
-use thiserror::Error;
+use clap::{error::ContextKind, error::ContextValue, error::ErrorKind, Parser, ValueEnum};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -24,7 +22,7 @@ pub struct Arg {
     #[arg(
         short,
         long,
-        value_parser = Color::from_str,
+        value_parser = ColorParser,
         required = false,
         required_if_eq("effect", "mono"),
     )]
@@ -167,35 +165,56 @@ impl fmt::Display for Color {
     }
 }
 
-#[derive(Error, Debug)]
-#[error("invalid color")]
-pub struct InvalidColor;
+#[derive(Clone)]
+struct ColorParser;
 
-impl std::str::FromStr for Color {
-    type Err = InvalidColor;
+impl clap::builder::TypedValueParser for ColorParser {
+    type Value = Color;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let color = s.to_lowercase();
-        if color.starts_with("#") {
-            if color.len() != 7 {
-                return Err(InvalidColor);
+    fn parse_ref(
+        &self,
+        cmd: &clap::Command,
+        arg: Option<&clap::Arg>,
+        value: &std::ffi::OsStr,
+    ) -> Result<Self::Value, clap::Error> {
+        let s = value
+            .to_str()
+            .ok_or_else(|| clap::Error::new(ErrorKind::InvalidUtf8).with_cmd(cmd))?;
+        if s.starts_with("#") {
+            if s.len() != 7 {
+                return Err(clap::Error::new(ErrorKind::ValueValidation));
             }
             if let (Ok(r), Ok(g), Ok(b)) = (
-                u8::from_str_radix(&color[1..3], 16),
-                u8::from_str_radix(&color[3..5], 16),
-                u8::from_str_radix(&color[5..7], 16),
+                u8::from_str_radix(&s[1..3], 16),
+                u8::from_str_radix(&s[3..5], 16),
+                u8::from_str_radix(&s[5..7], 16),
             ) {
                 return Ok(Color(r, g, b));
             } else {
-                return Err(InvalidColor);
+                return Err(clap::Error::new(ErrorKind::ValueValidation));
+            }
+        } else {
+            match s.to_lowercase().as_str() {
+                "red" => Ok(Color(0xff, 0x00, 0x00)),
+                "green" => Ok(Color(0x00, 0xff, 0x00)),
+                "blue" => Ok(Color(0x00, 0x00, 0xff)),
+                _ => Err(clap::Error::new(ErrorKind::ValueValidation)),
             }
         }
-        match color.as_str() {
-            "red" => Ok(Color(0xff, 0x00, 0x00)),
-            "green" => Ok(Color(0x00, 0xff, 0x00)),
-            "blue" => Ok(Color(0x00, 0x00, 0xff)),
-            _ => Err(InvalidColor),
-        }
+        .map_err(|err| {
+            let mut err = err.with_cmd(cmd);
+            if let Some(arg) = arg {
+                err.insert(
+                    ContextKind::InvalidArg,
+                    ContextValue::String(arg.to_string()),
+                );
+            }
+            err.insert(
+                ContextKind::InvalidValue,
+                ContextValue::String(s.to_string()),
+            );
+            err
+        })
     }
 }
 
